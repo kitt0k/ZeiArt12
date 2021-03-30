bc405ab6b84dc22688bc01ff9cb28d0b420ba874
wsi9 <collegenik@outlook.com>
2021-03-30 10:35:30 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZeiArt12
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void LastName_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (int.TryParse(e.Text, out val))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Name_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int d;
+            if (int.TryParse(e.Text, out d))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void Age_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            int w;
+            if (!int.TryParse(e.Text, out w))
+            {
+                e.Handled = true;
+            }
+        }
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Фамилия: {LastName.Text}\nИмя: {Name.Text}\nВозраст: {Age.Text}");
+        }
+
+
+        private void Users_Click(object sender, RoutedEventArgs e)
+        {
+            
+            using (StreamReader sr= new StreamReader("openusers.txt"))
+            {
+                for (int i = 0; i < File.ReadLines("openusers.txt").Count(); i++)
+                {
+                    string str = sr.ReadLine();
+                    if(str.ToString().Length>3)
+                    {
+                        MessageBox.Show(str);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Файл не выбран");
+                    }
+                }
+
+            }
+           
+
+        }
+
+
+    }
+}
+ // trash
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//OpenFileDialog openusers = new OpenFileDialog();
+//openusers.ShowDialog();
+//if (openusers.FileName != "")
+//{
+//    StreamReader sr = new StreamReader(@"C:\Users\WSR\Desktop\openusers.txt");
+//    while (!sr.EndOfStream)
+//    {
+//        string[] lines = new string[3];
+//        for (int i = 0; i < 3; i++)
+//        {
+//            string line = sr.ReadLine();
+//            string[] data = line.Split(':');
+//            line = "";
+//            if (data.Length >= 2)
+//            {
+//                for (int j = 0; j < data.Length; j++)
+//                {
+//                    line += data[j];
+//                }
+//            }
+
+//            lines[i] = line;
+
+//        }
+//        MessageBox.Show("Данные в файле:\n Фамилия: " + lines[0] + "\n Имя: " + lines[1] + "\n Возраст: " + lines[2]);
+//    }
+//}
+
+//else
+//{
+//    MessageBox.Show("Файл не выбран");
+//}
